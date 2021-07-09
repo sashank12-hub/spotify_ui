efa0c6f2095d346c9186eb1935c63867911a4957
sashank <chavalysashank@gmail.com>
2021-07-09 16:26:56 +0530

    Updates

--- src/components/api.js
+++
@@ -1,3 +0,0 @@
-export const newReleases='https://v1.nocodeapi.com/sashank/spotify/VnpekSLvumWmSCKa/browse/new?country=IN&perPage=30';
-export const browseCategories='https://v1.nocodeapi.com/sashank/spotify/VnpekSLvumWmSCKa/browse/categories?country=IN&locale=te&perPage=30';
-export const featuredPlaylists='https://v1.nocodeapi.com/sashank/spotify/VnpekSLvumWmSCKa/browse/featured?country=IN&locale=te&perPage=30'
